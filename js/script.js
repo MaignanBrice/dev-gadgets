d9bcdfac6e129cca2813487457173c89b5e5ae57
Brice Maignan <mgnn.brice@pm.me>
2023-09-19 09:54:57 +0200

    Added functions : AddCTA event, Add to cart, Disable AddCTA

--- js/script.js
+++ js/script.js
@@ -5,22 +5,45 @@ let imgID = 1; // IMG src counter.
 
 // -------Going to the next IMG
 document.querySelector('.pictures-next').addEventListener('click', event => {
-    imgID ++;
-    if(imgID === 6) imgID = 1;
+    imgID++;
+    if (imgID === 6) imgID = 1;
     document.querySelector('.pictures-img').src = `/img/canard-jaune-${imgID}-l.png`;
 })
 
 // -------Coming back to the previous IMG.
 document.querySelector('.pictures-prev').addEventListener('click', event => {
-    imgID --;
-    if(imgID === 0) imgID = 5;
+    imgID--;
+    if (imgID === 0) imgID = 5;
     document.querySelector('.pictures-img').src = `/img/canard-jaune-${imgID}-l.png`;
 })
 
 //  EventListener Desktop
 
-// Img hovering
+// -------Img hovering
 document.querySelector('.thumbs').addEventListener('mouseover', event => {
-    if(!event.target.classList.contains('thumbs-img')) return;
+    if (!event.target.classList.contains('thumbs-img')) return;
     document.querySelector('.pictures-img').src = event.target.dataset.imgLargeSrc;
+});
+
+// Clic on ADD button
+
+document.querySelector('.add-cta').addEventListener('click', event => {
+    if (event.target.classList.contains('add-cta')) {
+        addItemToCart();
+        changeCtaStyle(event.target);
+    }
 })
+
+
+// Display cart item number from 1 to 99+ 
+function addItemToCart() {
+    let quantity = parseInt(document.querySelector('.add-qty').value);
+    quantity > 99 ? document.querySelector('.cart-nb').textContent = '99+' : document.querySelector('.cart-nb').textContent = quantity;
+}
+
+// Change CTA Css to display unavailable click.
+function changeCtaStyle(node) {
+    node.classList.remove('add-cta');
+    node.classList.add('add-cta-clicked');
+    node.textContent = 'Déjà au panier';
+}
