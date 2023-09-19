ae8b926f8fc4bc382cdf542c40fa715b726e66bb
Brice Maignan <mgnn.brice@pm.me>
2023-09-19 10:54:10 +0200

    Setting localstorage for accordion state and setting this state on load

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 
 let imgID = 1; // IMG src counter.
 
-// EventListener Boutons Mobile
+
+// ON LOAD - EventListener
+
+window.addEventListener('load', event => {
+    setAccordionStateOnLoad();
+})
+
+
+// EventListener Mobile BTN
 
 // -------Going to the next IMG
 document.querySelector('.pictures-next').addEventListener('click', event => {
@@ -25,7 +33,7 @@ document.querySelector('.thumbs').addEventListener('mouseover', event => {
     document.querySelector('.pictures-img').src = event.target.dataset.imgLargeSrc;
 });
 
-// Clic on ADD button
+// -------Clic on ADD button
 
 document.querySelector('.add-cta').addEventListener('click', event => {
     if (event.target.classList.contains('add-cta')) {
@@ -34,16 +42,48 @@ document.querySelector('.add-cta').addEventListener('click', event => {
     }
 })
 
+// -------Accordions
 
-// Display cart item number from 1 to 99+ 
+document.querySelector('.page-txt').addEventListener('click', event => {
+    if (event.target.id !== 'advantages-accordion' && event.target.id !== 'features-accordion') return;
+    displayAccordion(event.target);
+})
+
+
+
+// Functions
+
+// -------Display cart item number from 1 to 99+ 
 function addItemToCart() {
     let quantity = parseInt(document.querySelector('.add-qty').value);
     quantity > 99 ? document.querySelector('.cart-nb').textContent = '99+' : document.querySelector('.cart-nb').textContent = quantity;
 }
 
-// Change CTA Css to display unavailable click.
+// -------Disable add-cta and change display
 function changeCtaStyle(node) {
     node.classList.remove('add-cta');
     node.classList.add('add-cta-clicked');
     node.textContent = 'Déjà au panier';
+}
+
+// -------Toggle hidden class for accordions
+function displayAccordion(node) {
+    node.nextElementSibling.classList.toggle('hidden');
+    saveAccordionState();
+}
+// -------Local Store the accordion state
+function saveAccordionState() {
+    let advantagesClasses = document.querySelector('.product-advantages').classList;
+    localStorage.setItem('advantages', advantagesClasses.toString())
+    let featuresClasses = document.querySelector('.product-car').classList;
+    localStorage.setItem('features', featuresClasses.toString())
+}
+
+// ------- Load Accordion state from Local Storage
+function setAccordionStateOnLoad() {
+    let advantagesClasses = (localStorage.getItem('advantages')).split(' ');
+    let featuresClasses = localStorage.getItem('features').split(' ');
+
+    advantagesClasses.map(item => document.querySelector('.product-advantages').classList.add(item));
+    featuresClasses.map(item => document.querySelector('.product-car').classList.add(item));
 }
