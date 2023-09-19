f930ff4c623c75dddb482b6abc02cd450841420e
Brice Maignan <mgnn.brice@pm.me>
2023-09-19 13:49:40 +0200

    Clearing code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 let imgID = 1; // IMG src counter.
-let similarCnt = 0;
+let similarCnt = 0; // Similar Mobile Counter
 
 //////////////////// ON LOAD - EventListener /////////////////
 
 window.addEventListener('load', event => {
     setAccordionStateOnLoad();
-})
+});
 
 // //////////////////EventListener Mobile BTN //////////////////
 
@@ -14,14 +14,14 @@ document.querySelector('.pictures-next').addEventListener('click', event => {
     imgID++;
     if (imgID === 6) imgID = 1;
     document.querySelector('.pictures-img').src = `/img/canard-jaune-${imgID}-l.png`;
-})
+});
 
 // -------Coming back to the previous IMG.
 document.querySelector('.pictures-prev').addEventListener('click', event => {
     imgID--;
     if (imgID === 0) imgID = 5;
     document.querySelector('.pictures-img').src = `/img/canard-jaune-${imgID}-l.png`;
-})
+});
 
 // ------- Previous BTN Similar Section
 
@@ -55,10 +55,8 @@ document.querySelector('.similar').addEventListener('click', event => {
             list[similarCnt].classList.remove('slide-out-left');
             similarCnt--;
             event.target.classList.toggle('hidden');
-        }, 1000)
-
-    }
-    return;
+        }, 1000);
+    } return;
 });
 
 // ------- Next BTN Similar Section
@@ -67,7 +65,6 @@ document.querySelector('.similar').addEventListener('click', event => {
     if (!event.target.classList.contains('similar-btn-next')) return;
 
     let list = document.querySelectorAll('.similar-itm');
-
 
     if (similarCnt === 2) {
 
@@ -92,9 +89,7 @@ document.querySelector('.similar').addEventListener('click', event => {
             similarCnt++;
         }, 1000);
 
-
         document.querySelector('.similar-btn-prev').classList.remove('hidden');
-
     } return;
 });
 
@@ -113,15 +108,15 @@ document.querySelector('.add-cta').addEventListener('click', event => {
     if (event.target.classList.contains('add-cta')) {
         addItemToCart();
         changeCtaStyle(event.target);
-    }
-})
+    };
+});
 
 // -------Accordions
 
 document.querySelector('.page-txt').addEventListener('click', event => {
     if (event.target.id !== 'advantages-accordion' && event.target.id !== 'features-accordion') return;
     displayAccordion(event.target);
-})
+});
 
 
 
@@ -131,55 +126,54 @@ document.querySelector('.page-txt').addEventListener('click', event => {
 function addItemToCart() {
     let quantity = parseInt(document.querySelector('.add-qty').value);
     quantity > 99 ? document.querySelector('.cart-nb').textContent = '99+' : document.querySelector('.cart-nb').textContent = quantity;
-}
+};
 
 // -------Disable add-cta and change display
 function changeCtaStyle(node) {
     node.classList.remove('add-cta');
     node.classList.add('add-cta-clicked');
     node.textContent = 'Déjà au panier';
-}
+};
 
 // -------Toggle hidden class for accordions
 function displayAccordion(node) {
     node.nextElementSibling.classList.toggle('hidden');
     saveAccordionState();
-}
+};
+
 // -------Local Store the accordion state
 function saveAccordionState() {
     let advantagesClasses = document.querySelector('.product-advantages').classList;
     localStorage.setItem('advantages', advantagesClasses.toString())
     let featuresClasses = document.querySelector('.product-car').classList;
     localStorage.setItem('features', featuresClasses.toString())
-}
+};
 
 // ------- Load Accordion state from Local Storage
 function setAccordionStateOnLoad() {
     if (localStorage.getItem('advantages') === null && localStorage.getItem('features') === null) return;
-    
     let advantagesClasses = (localStorage.getItem('advantages')).split(' ');
     let featuresClasses = localStorage.getItem('features').split(' ');
 
     advantagesClasses.map(item => document.querySelector('.product-advantages').classList.add(item));
     featuresClasses.map(item => document.querySelector('.product-car').classList.add(item));
-
-}
+};
 
 // ------- Toggle 'mobile-hidden' class
 function toggleMobileHiddenClass(node) {
     node.classList.toggle('mobile-hidden');
-}
+};
 
 // ------- Add 'slide-in-left' Remove 'slide-in-right'
 function addLeftRemoveRight(node) {
     node.classList.add('slide-in-left');
     node.classList.remove('slide-in-right');
-}
+};
 
 // ------- Remove 'slide-in-left' Add 'slide-in-right'
 function addRightRemoveLeft(node) {
     node.classList.remove('slide-in-left');
     node.classList.add('slide-in-right');
-}
+};
 
 
